ce1d6d7465d763fe973c674bbf314cabee94b0bd
Thomas Sankara <thomassankara74@gmail.com>
2021-07-15 10:44:59 +0300

    initial design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,71 @@
+import logo from "./images/logo.svg"
+import dollar from "./images/icon-dollar.svg"
+import person from "./images/icon-person.svg"
+
 const App = () => {
   return (
     <>
-      <h1>Tip Calculator App</h1>
+      <main>
+        <div>
+          <img src={logo} alt="splitter" />
+        </div>
+        <section className="container">
+          <form>
+            <label htmlFor="bill">
+              <img src={dollar} alt="" />
+            </label>
+            <input
+              type="number"
+              id="bill"
+              name="bill"
+              placeholder="0.0"
+              required
+            />
+          </form>
+
+          <div className="tip">
+            <p>Select Tip %</p>
+            <div className="buttons">
+              <button>5%</button>
+              <button>10%</button>
+              <button>15%</button>
+              <button>25%</button>
+              <button>50%</button>
+              <button>Custom</button>
+            </div>
+          </div>
+
+          <form>
+            <p>Number of people</p>
+            <label htmlFor="person">
+              <img src={person} alt="" />
+            </label>
+            <input
+              type="number"
+              name="person"
+              id="person"
+              required
+              placeholder="1"
+            />
+          </form>
+
+          <div className="amount">
+            <article>
+              <h3>
+                Tip amount <span>/ person</span>
+              </h3>
+              <p>$4.27</p>
+            </article>
+            <article>
+              <h3>
+                Total <span>/ person</span>
+              </h3>
+              <p>$32.79</p>
+            </article>
+            <input type="reset" value="RESET" />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
